a31f18724c07533d9ff622a40b09f96bece41ebb
kjkondo <kkondo57@gmail.com>
2023-04-19 17:36:20 -0700

    ssss

--- web/src/App.js
+++ web/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://i.imgur.com/AD3MbBi.jpeg" className="App-logo" alt="logo" />
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="https://i.imgur.com/AD3MbBi.jpeg" className="App-logo" alt="logo" /></a>
         <p>
           Give me money
         </p>
