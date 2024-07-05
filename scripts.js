39ad9c4fb38bdb3f889999f468523fb9bed610f6
kjkondo <kmbp@Kyos-MBP.attlocal.net>
2024-07-05 02:05:49 -0700

    should now dynamically load posts

--- scripts.js
+++ scripts.js
@@ -2,7 +2,22 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const postsContainer = document.getElementById("posts-container");
-  const postFiles = ["post1.html", "post2.html"]; // Add more post files as needed
+  let postFiles = []; // Add more post files as needed
+  const apiUrl = `https://api.github.com/repos/kjkondo/kjkondo.github.io/contents/posts`;
+
+  // Create a synchronous XMLHttpRequest
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", apiUrl, false); // false makes the request synchronous
+  xhr.send();
+
+  if (xhr.status !== 200) {
+    throw new Error(`HTTP error! Status: ${xhr.status}`);
+  }
+
+  const data = JSON.parse(xhr.responseText);
+
+  // Extract file names
+  postFiles = data.map((item) => "./" + item.name);
 
   postFiles.forEach((postFile) => {
     fetch(`./posts/${postFile}`)
